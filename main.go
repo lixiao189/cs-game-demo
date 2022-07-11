402aed612fca91723f32e4d66841364738a6d7fe
Node Sans <node189@icloud.com>
2022-07-11 23:38:41 +0800

    programme flags added

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/jessevdk/go-flags"
 	"github.com/lixiao189/cs-game-demo/client/game"
 )
 
+var opts struct {
+	// Slice of bool will append 'true' each time the option
+	// is encountered (can be set multiple times, like -vvv)
+	Deamon bool `short:"d" long:"deamon" description:"Whether working on deamon server"`
+}
+
 func main() {
-	// Client initialize and startup code
-	game := game.Game{
-		Height: 640,
-		Width:  1024,
+	// Parse flags
+	_, err := flags.ParseArgs(&opts, os.Args)
+	if err != nil {
+		os.Exit(0)
 	}
-	game.InitGame()
-	if err := ebiten.RunGame(&game); err != nil {
-		log.Fatal(err)
+
+	if opts.Deamon {
+		log.Println("Running on server mode")
+		
+	} else {
+		// Client initialize and startup code
+		game := game.Game{
+			Height: 640,
+			Width:  1024,
+		}
+		game.InitGame()
+		if err := ebiten.RunGame(&game); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
-.PHONY: client run-client
+.PHONY: client run-client run-server
 
 build:
 	go build -o game.out
 
 run-client: 
 	go run main.go
+
+run-server:
+	go run main.go -d
